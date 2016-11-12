e43056e91934b35a28467c1e1c7a1fc8649cfef5
Jonas <jojonas@gmail.com>
2016-11-12 14:59:13 +0100

    Replace

--- src/scrapeEmailer/scrape/flippermarkt.js
+++ src/scrapeEmailer/scrape/flippermarkt.js
@@ -1,7 +1,15 @@
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({name: 'pinscraper', module: 'flippermarkt'});
-var Nightmare = require('nightmare');
-var nightmare = Nightmare();
+var cheerio = require('cheerio');
+var rp = require('request-promise');
+// var Nightmare = require('nightmare');
+// var nightmare = Nightmare();
+var options = {
+  uri: 'http://flippermarkt.de/community/forum/forumdisplay.php?f=13',
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
 
 function scrape (pins) {
   if (!pins) {
@@ -10,40 +18,30 @@ function scrape (pins) {
 
   var site = 'Flippermarkt';
   log.info({ event: 'scrape', site, pins });
-  return nightmare
-    .goto('http://flippermarkt.de/community/forum/forumdisplay.php?f=13')
-    .wait('#threadbits_forum_13')
-    .evaluate(function (pins) {
+  return rp(options)
+    .then($ => {
       var matches = [];
-      var elems = document.querySelectorAll('#threadbits_forum_13 tr td[id*="td_threadtitle"]');
-      for (var i = 0; i < elems.length; i++) {
-        var elem = elems[i];
-        var title = elem && elem.title;
-        var elemHref = elem.querySelector('a[id*="thread_title"]');
-        var hrefText = elemHref && elemHref.innerHTML;
+      $('#threadbits_forum_13 tr td[id*="td_threadtitle"]').each(function (i, elem) {
+        var title = elem.attribs['title'];
+        var elemHref = $(elem).find('a[id*="thread_title"]');
+        var hrefText = elemHref && elemHref.text();
         if (title || hrefText) {
           pins.forEach(pin => {
             if (title && title.toLowerCase().indexOf(pin.toLowerCase()) !== -1 ||
                 hrefText && hrefText.toLowerCase().indexOf(pin.toLowerCase()) !== -1) {
-              var a = elem.querySelector('a');
+              var a = $(elem).find('a')[0];
               matches.push({
+                site,
                 pin,
                 title,
                 hrefText,
-                href: a && a.href
+                href: a && a.attribs['href']
               });
             }
           });
         }
-      }
-
-      return matches;
-    }, pins)
-    .end()
-    .then(matches => {
-      matches.forEach(match => {
-        match.site = site;
       });
+
       return matches;
     })
     .catch(err => {
