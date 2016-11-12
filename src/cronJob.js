35d6a1d4a25739511b12ac9dab9d82336493093e
Jonas <jojonas@gmail.com>
2016-11-12 20:52:19 +0100

    Set cron pattern to each 6 hours (starting at 2) every day

--- src/cronJob.js
+++ src/cronJob.js
@@ -6,9 +6,9 @@ var log = bunyan.createLogger({name: 'pinscraper webserver'});
 var jsonObj = jsonfile.readFileSync('./recipients.json');
 
 log.info({ event: 'pin-scraper-create-cron' });
-// Run every 6 hours
+// Run at 3 each 6 hours every day
 var job = new CronJob(
-  '0 0 0/6 * * *',
+  '0 3/6 * * * *',
   function () {
     try {
       log.info({ event: 'pin-scraper-cron-start' });
