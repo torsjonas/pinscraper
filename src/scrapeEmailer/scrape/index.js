afe271cafa855bf5c9c7566740668365396f0c73
Jonas <jojonas@gmail.com>
2016-11-13 19:30:05 +0100

    Add a scraper for site svenskaflippersallskapet.com

--- src/scrapeEmailer/scrape/index.js
+++ src/scrapeEmailer/scrape/index.js
@@ -1,7 +1,9 @@
 var flippermarkt = require('./flippermarkt');
+var sfss = require('./svenskaflippersallskapet');
+var scrapers = [flippermarkt, sfss];
 
 var scrape = function (pins) {
-  return Promise.all([flippermarkt].map(scraper => scraper.scrape(pins)))
+  return Promise.all(scrapers.map(scraper => scraper.scrape(pins)))
     .then(scrapeRequests => {
       return scrapeRequests.reduce((total, scrapeMatches) => {
         return total.concat(scrapeMatches);
---
+++ src/scrapeEmailer/scrape/svenskaflippersallskapet.js
@@ -0,0 +1,50 @@
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name: 'pinscraper', module: 'svenskaflippersallskapet'});
+var cheerio = require('cheerio');
+var rp = require('request-promise');
+
+var options = {
+  uri: 'http://www.svenskaflippersallskapet.com/index.php?board=7.0',
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
+
+function scrape (pins) {
+  if (!pins) {
+    return Promise.resolve([]);
+  }
+
+  var site = 'Svenska Flippersällskapet';
+  log.info({ event: 'scrape', site, pins });
+  return rp(options)
+    .then($ => {
+      var matches = [];
+      $('#messageindex.tborder.topic_table tr td.subject.windowbg2 span a').each(function (i, elem) {
+        var text = $(elem).text();
+        if (text) {
+          pins.forEach(pin => {
+            if (text.toLowerCase().indexOf(pin.toLowerCase()) !== -1) {
+              var matchUri = elem.attribs['href'] || null;
+              matches.push({
+                site,
+                pin,
+                text,
+                matchUri
+              });
+            }
+          });
+        }
+      });
+
+      return matches;
+    })
+    .catch(err => {
+      log.error({ event: 'scrape-error-svenskaflippersallskapet', err, message: err.message });
+      return [];
+    });
+}
+
+module.exports = {
+  scrape
+};
