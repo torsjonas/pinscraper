e081d6a1fc5c72d200e61d0e80f20d3df677aa8a
Jonas <jojonas@gmail.com>
2016-11-14 22:44:11 +0100

    Log recipients json on start (validation)

--- src/cronJob.js
+++ src/cronJob.js
@@ -3,17 +3,16 @@ var CronJob = require('cron').CronJob;
 var scrapeEmailer = require('./scrapeEmailer');
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({name: 'pinscraper webserver'});
-var jsonObj = jsonfile.readFileSync('./config/recipients.json');
+var recipients = jsonfile.readFileSync('./config/recipients.json').recipients;
+log.info({ event: 'pin-scraper-create-cron', recipients });
 
-log.info({ event: 'pin-scraper-create-cron' });
 // Run every hour
 var job = new CronJob(
   '0 0 0-23 * * *',
   function () {
     try {
       log.info({ event: 'pin-scraper-cron-start' });
-
-      scrapeEmailer.run(jsonObj.recipients);
+      scrapeEmailer.run(recipients);
     } catch (err) {
       log.error({ event: 'pin-scraper-cron-error', err });
     }
